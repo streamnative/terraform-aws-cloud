693be26993afdbfe9fa8290714030684c51c26e8
Joey Davenport <jungle.bird@gmail.com>
2021-08-06 14:00:45 -0600

    example update

--- examples/example-with-vpc/main.tf
+++ examples/example-with-vpc/main.tf
@@ -40,20 +40,45 @@ terraform {
 ### These data sources are required by the Kubernetes and Helm providers in order to connect to the newly provisioned cluster
 #######
 data "aws_eks_cluster" "cluster" {
-  name = module.eks_cluster.eks_cluster_id
+  name = module.sn_cluster.eks_cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster.eks_cluster_id
+  name = module.sn_cluster.eks_cluster_id
 }
 
+data "aws_caller_identity" "current" {}
+
+#######
+### The "random_pet" resource and locals block assist in building out the Cluster Name, as well the variables defined
+#######
+
+variable "environment" {
+  default = "test"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+resource "random_pet" "cluster_name" {
+  length = 1
+}
+
+locals {
+  account_id   = data.aws_caller_identity.current.account_id
+  cluster_name = format("sn-%s-%s-%s", random_pet.cluster_name.id, var.environment, var.region)
+}
+
+#######
+### The providers can be configured to dynamically retrieve the cluster connection configuration after it's been created
+#######
 provider "aws" {
   region = var.region
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "/path/to/my-sn-platform-cluster-config" # This must match the module input
+    config_path = pathexpand("~/.kube/${local.cluster_name}-config")
   }
 }
 
@@ -62,24 +87,33 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   insecure               = false
-  config_path            = "/path/to/my-sn-platform-cluster-config" # add this path to the provider configs above
+  config_path            = pathexpand("~/.kube/${local.cluster_name}-config")
 }
 
 module "vpc" {
-  source   = "streamnative/cloud/aws//modules/vpc"
-  vpc_cidr = "10.80.0.0/16"
+  source = "streamnative/cloud/aws//modules/vpc"
 
-  region = var.region
+  num_azs  = 3              # The number of availabiltiy zones to create.
+  vpc_cidr = "10.80.0.0/16" # The module will automatically create subnets based on this cidr and assign them to their respective AZs.
+  vpc_name = local.cluster_name
+  region   = var.region
 }
 
-module "sn_platform_cluster" {
+module "sn_cluster" {
   source = "streamnative/cloud/aws"
 
-  cluster_name           = "my-sn-platform-cluster"
-  cluster_version        = "1.19"
-  kubeconfig_output_path = "/path/to/my-sn-platform-cluster-config"
+  add_vpc_tags             = true # This will add the necessary tags to the VPC resources for Ingress controller auto-discovery 
+  cluster_name             = local.cluster_name
+  cluster_version          = "1.19"
+  hosted_zone_id           = "Z04554535IN8Z31SKDVQ2" # Change this to your hosted zone ID
+  kubeconfig_output_path   = pathexpand("~/.kube/${local.cluster_name}-config")
+  func_pool_instance_types = ["t3.medium"]
+  node_pool_instance_types = ["m5.large"]
+  node_pool_desired_size   = 3
+  node_pool_min_size       = 1
+  node_pool_max_size       = 3
 
-  map_additional_iam_roles = [
+  map_additional_iam_roles = [ # Map your IAM admin role for access within the Cluster
     {
       rolearn  = "arn:aws:iam::123456789012:role/my-aws-admin-role"
       username = "management-admin"
@@ -87,20 +121,16 @@ module "sn_platform_cluster" {
     }
   ]
 
-  node_pool_instance_types = ["m4.large"]
-  node_pool_desired_size   = 3
-  node_pool_min_size       = 3
-  node_pool_max_size       = 5
-  pulsar_namespace         = "pulsar-demo"
+  private_subnet_ids      = module.vpc.private_subnet_ids # Use the list of private subnets created by the VPC module
+  public_subnet_ids       = module.vpc.public_subnet_ids  # Use the list of public subnets created by the VPC module
+  pulsar_namespace        = "pulsar-demo"                 # The namespace you anticipate using for Pulsar
+  pulsar_namespace_create = true                          # Have the module create the Pulsar namespace. This is recommended. 
+  region                  = var.region
+  s3_bucket_name_override = format("%s-offload", local.cluster_name) # Use this to override the default S3 bucket name for tiered storage offloading
+  vault_prefix_override   = local.cluster_name                       # Use this to override the default Vault prefix for Vault resources
+  vpc_id                  = module.vpc.vpc_id                        # Use the VPC ID created by the VPC module
 
-  hosted_zone_id     = "Z04554535IN8Z31SKDVQ2"
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
-  region             = var.region
-  vpc_id             = module.vpc.vpc_id
-
-  # Adding a dependency on the VPC module allows for a clean destroy
   depends_on = [
-    module.vpc
+    module.vpc # Adding a dependency on the VPC module allows for a cleaner destroy
   ]
 }
