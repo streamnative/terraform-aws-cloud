0816fafbec7ada072f020a8d6096a72aa2fc4aaf
Joey Davenport <jrsdav@users.noreply.github.com>
2022-01-24 16:29:19 -0700

    Removed duplicate provider config

--- modules/managed-cloud/main.tf
+++ modules/managed-cloud/main.tf
@@ -17,17 +17,6 @@
 # under the License.
 #
 
-terraform {
-  required_version = ">=1.0.0"
-
-  required_providers {
-    aws = {
-      version = ">=3.61.0"
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "streamnative_vendor_access" {
