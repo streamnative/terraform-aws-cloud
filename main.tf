90fe905c7b0652218a619e5c0b5075c5daa9a049
John Daniel Maguire <john.maguire@streamnative.io>
2022-08-09 13:02:26 -0300

    Update eks terraform version

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.24.0"
+  version = "18.26.6"
 
   cluster_name                                   = var.cluster_name
   cluster_version                                = var.cluster_version
