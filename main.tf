fcdce2154f1dbd25df8caf1bcce5c1f2842d1586
John Daniel Maguire <john.maguire@streamnative.io>
2023-01-31 19:15:04 -0400

    Bump to 19.x

--- main.tf
+++ main.tf
@@ -163,7 +163,7 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.30.2" #"19.6.0"
+  version = "19.6.0"
 
   ######################################################################################################
   ### This section takes into account the breaking changes made in v18.X of the community EKS module ###
