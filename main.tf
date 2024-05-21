74d2fc320b260337c24a3aba67705b44182afebc
Yisheng Cai <yisheng.cai@streamnative.io>
2024-05-21 15:47:51 +0800

    Fix

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_subnet" "private_subnets" {
 }
 
 data "aws_subnet" "public_subnets" {
-  count = length(var.private_subnet_ids)
+  count = length(var.public_subnet_ids)
   id    = var.public_subnet_ids[count.index]
 }
 
