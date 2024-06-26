250318f5f9ebc8ae57b2a32d04a1c9d38bbe1238
Max Xu <xuhuan@live.cn>
2024-06-26 22:43:20 +0800

    feat: add availability_zones to vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,6 +16,11 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals {
+  azs     = length(var.availability_zones) > 0 ? var.availability_zones : data.aws_availability_zones.available.names
+  num_azs = length(var.availability_zones) > 0 ? length(var.availability_zones) : var.num_azs
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -28,11 +33,11 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "public" {
-  count = var.num_azs
+  count = local.num_azs
 
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, var.public_subnet_newbits, var.public_subnet_start + count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = local.azs[count.index]
   map_public_ip_on_launch = var.disable_nat_gateway ? true : var.public_subnet_auto_ip
   tags                    = merge({ "Vendor" = "StreamNative", "Type" = "public", "kubernetes.io/role/elb" = "1", Name = format("%s-public-sbn-%s", var.vpc_name, count.index) }, var.tags)
 
@@ -42,11 +47,11 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = var.num_azs
+  count = local.num_azs
 
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, var.private_subnet_newbits, var.private_subnet_start + count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone = local.azs[count.index]
   tags              = merge({ "Vendor" = "StreamNative", "Type" = "private", "kubernetes.io/role/internal-elb" = "1", Name = format("%s-private-sbn-%s", var.vpc_name, count.index) }, var.tags)
 
   lifecycle {
@@ -64,7 +69,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_eip" "eip" {
-  count = var.disable_nat_gateway ? 0 : var.num_azs
+  count = var.disable_nat_gateway ? 0 : local.num_azs
 
   domain = "vpc"
   tags   = merge({ "Vendor" = "StreamNative", Name = format("%s-eip-%s", var.vpc_name, count.index) }, var.tags)
@@ -76,7 +81,7 @@ resource "aws_eip" "eip" {
 }
 
 resource "aws_nat_gateway" "nat_gw" {
-  count = var.disable_nat_gateway ? 0 : var.num_azs
+  count = var.disable_nat_gateway ? 0 : local.num_azs
 
   allocation_id = aws_eip.eip[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
@@ -107,14 +112,14 @@ resource "aws_route" "public_route" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count = var.num_azs
+  count = local.num_azs
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_route_table[0].id
 }
 
 resource "aws_route_table" "private_route_table" {
-  count = var.disable_nat_gateway ? 0 : var.num_azs
+  count = var.disable_nat_gateway ? 0 : local.num_azs
 
   vpc_id = aws_vpc.vpc.id
   tags   = merge({ "Vendor" = "StreamNative", Name = format("%s-private-rtb-%s", var.vpc_name, count.index) }, var.tags)
@@ -125,7 +130,7 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route" "private_route" {
-  count = var.disable_nat_gateway ? 0 : var.num_azs
+  count = var.disable_nat_gateway ? 0 : local.num_azs
 
   route_table_id         = aws_route_table.private_route_table[count.index].id
   nat_gateway_id         = aws_nat_gateway.nat_gw[count.index].id
@@ -133,7 +138,7 @@ resource "aws_route" "private_route" {
 }
 
 resource "aws_route_table_association" "private_assoc" {
-  count = var.disable_nat_gateway ? 0 : var.num_azs
+  count = var.disable_nat_gateway ? 0 : local.num_azs
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_route_table[count.index].id
