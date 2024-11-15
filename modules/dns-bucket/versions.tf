624864f9afc15017cf8d5ad5a515410b2df5df4a
Max Xu <xuhuan@live.cn>
2024-11-15 16:28:44 +0800

    docs: update tf docs

--- modules/dns-bucket/versions.tf
+++ modules/dns-bucket/versions.tf
@@ -1,18 +1,29 @@
+# Copyright 2023 StreamNative, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 terraform {
   required_version = ">=1.2.0"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # NOTE! we required two different providers in this module
-      # this is because we need to create a zone in the target
-      # and then create the delegations in the source
+      # NOTE! we required two different providers in this module this is because we need to create a zone in the target and then create the delegations in the source
       configuration_aliases = [aws.target, aws.source]
     }
   }
 }
 
-// NOTE! we need to create these so terraform validate works (even though it seems to be deperacted)
 provider "aws" {
   alias = "target"
 }
