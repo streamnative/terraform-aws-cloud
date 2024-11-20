8b7202b17bb7abe4e0f84e7785efbe1fa4c42635
Max Xu <xuhuan@live.cn>
2024-11-20 11:24:27 +0800

    feat: add module for dns and bucket (#145)
    <!--
      ~ Copyright 2023 StreamNative, Inc.
      ~
      ~ Licensed under the Apache License, Version 2.0 (the "License");
      ~ you may not use this file except in compliance with the License.
      ~ You may obtain a copy of the License at
      ~
      ~     http://www.apache.org/licenses/LICENSE-2.0
      ~
      ~ Unless required by applicable law or agreed to in writing, software
      ~ distributed under the License is distributed on an "AS IS" BASIS,
    ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
    implied.
      ~ See the License for the specific language governing permissions and
      ~ limitations under the License.
    -->
    
    <!--
    ### Contribution Checklist
    
    - Fill out the template below to describe the changes contributed by the
    pull request. That will give reviewers the context they need to do the
    review.
      
    - Each pull request should address only one issue, not mix up code from
    multiple issues.
      
      - Each commit in the pull request has a meaningful commit message
    
    - Once all items of the checklist are addressed, remove the above text
    and this checklist, leaving only the filled out template below.
    
    **(The sections below can be removed for hotfixes of typos)**
    -->
    
    ### Motivation
    
    For support StreamNative Cloud.
    
    ### Modifications
    
    - Add new submodule for creating dns zone and s3 bucket
    
    It will backport as `v3.4.0` in
    https://github.com/streamnative/terraform-aws-cloud/tree/release/v3.4.0
    
    ### Verifying this change
    
    - [x] Verified
    
    
    ### Documentation
    
    - [x] `doc`

---
+++ modules/dns-bucket/outputs.tf
@@ -0,0 +1,33 @@
+# Copyright 2023 StreamNative, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+output "zone_id" {
+  value = local.zone_id
+}
+
+output "zone_name" {
+  value = local.zone_name
+}
+
+output "backup_bucket" {
+  value = aws_s3_bucket.velero.bucket
+}
+
+output "backup_bucket_kms_key_id" {
+  value = local.s3_kms_key
+}
+
+output "tiered_storage_bucket" {
+  value = aws_s3_bucket.tiered_storage.bucket
+}
---
+++ modules/dns-bucket/variables.tf
@@ -0,0 +1,53 @@
+# Copyright 2023 StreamNative, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "pm_name" {
+  description = "The name of the poolmember, for new clusters, this should be like `pm-<xxxxx>`"
+  type        = string
+}
+
+variable "parent_zone_name" {
+  type        = string
+  description = "The parent zone in which we create the delegation records"
+}
+
+variable "custom_dns_zone_id" {
+  type        = string
+  default     = ""
+  description = "if specified, then a streamnative zone will not be created, and this zone will be used instead. Otherwise, we will provision a new zone and delegate access"
+}
+
+variable "custom_dns_zone_name" {
+  type        = string
+  default     = ""
+  description = "must be passed if custom_dns_zone_id is passed, this is the zone name to use"
+}
+
+variable "s3_encryption_kms_key_arn" {
+  default     = ""
+  description = "KMS key ARN to use for S3 encryption. If not set, the default AWS S3 key will be used."
+  type        = string
+}
+
+variable "extra_aws_tags" {
+  default     = {}
+  description = "Additional to apply to the resources. Note that this module sets the tags Name, Type, and Vendor by default. They can be overwritten, but it is not recommended."
+  type        = map(string)
+}
+
+locals {
+  tags = merge({
+    "Vendor" = "StreamNative"
+  }, var.extra_aws_tags)
+}
