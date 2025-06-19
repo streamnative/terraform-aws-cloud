c2531cd69cf1022bf2129d368057c2f20f9f558d
Yisheng Cai <yisheng.cai@streamnative.io>
2025-06-19 13:46:08 +0800

    Declare version requirements in example

--- examples/sn-cluster/main.tf
+++ examples/sn-cluster/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = "1.5.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+  }
+}
 data "aws_caller_identity" "current" {}
 
 data "aws_partition" "current" {}
