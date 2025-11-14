4d1a05f49634dd033dda38f0096b51b11ce9addd
ignacioli <yqli@streamnative.io>
2025-11-14 19:17:41 +0800

    enable creating bucket for loki and remove useless tags

--- modules/dns-bucket/bucket.tf
+++ modules/dns-bucket/bucket.tf
@@ -27,10 +27,9 @@ resource "aws_s3_bucket" "tiered_storage" {
 }
 
 resource "aws_s3_bucket" "loki" {
-  count         = var.enable_loki ? 1 : 0
   provider      = aws.source
   bucket        = format("loki-%s-%s", var.pm_namespace, var.pm_name)
-  tags          = merge({ "Attributes" = "loki", "Name" = "logs-byoc" }, local.tags)
+  tags          = merge({ "Attributes" = "loki" }, local.tags)
   force_destroy = true
 }
 
